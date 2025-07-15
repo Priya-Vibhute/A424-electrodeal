215c6ef568f09eaf9a4548ba0cecfce00949b5d1
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-07-15 07:16:20 +0530

    14 july

--- electrodeal/views.py
+++ electrodeal/views.py
@@ -1,16 +1,25 @@
 
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 def home(request):
     return render(request,"index.html")
 
 def register(request):
-    form=UserCreationForm()
-    context={
-        "form":form
-    }
-    return render(request,"register.html",context)
+    if request.method=="GET":
+        form=CustomUserCreationForm()
+        context={
+            "form":form
+        }
+        return render(request,"register.html",context)
+    elif request.method=="POST":
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request,"register.html")
+
+       
+
 
 def login(request):
     context={}
